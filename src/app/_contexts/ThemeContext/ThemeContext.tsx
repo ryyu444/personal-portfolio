a51a17753e5e840c59d543e7b6dbdeb981d4e21d
Ryan <ryyu444@gmail.com>
2025-07-07 17:42:50 -0700

    added theme context

---
+++ src/app/_contexts/ThemeContext/ThemeContext.tsx
@@ -0,0 +1,37 @@
+/* 
+    ThemeContext is a context that provides the theme to the app.
+    - Stores theme in context
+    - Provides updating capabilities for theme (light/dark)
+*/
+
+import { createContext, useContext, useState } from 'react';
+
+interface ThemeContextType {
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// Initial Theme Context; Note: setTheme should not be called unless in ThemeProvider
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: 'light',
+  setTheme: () => {},
+});
+
+// Custom hook to access theme context
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('No theme context found');
+  }
+  return context;
+}
+
+// Light/Dark theme provider
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './_components/(navigation)/navbar/Navbar';
 import Footer from './_components/(navigation)/footer/Footer';
+import { ThemeProvider } from './_contexts/ThemeContext/ThemeContext';
 import './globals.css';
 
 const inter = Inter({
@@ -11,7 +12,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Ryan's Portfolio",
-  description: 'A showcase of me and my work',
+  description: 'A website showcasing my work and skills',
 };
 
 export default function RootLayout({
@@ -22,9 +23,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.variable} antialiased`}>
-        <Navbar />
-        {children}
-        <Footer />
+        <ThemeProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
